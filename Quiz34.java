835e29fffdd132e0551b8d294e38ebec1452fcb7
Manpreet <Manpreet@DESKTOP-8VTP474>
2019-05-28 16:13:42 -0800

    part 1

---
+++ Quiz34.java
@@ -0,0 +1,9 @@
+
+public class Quiz34 {
+	public static void main(String Args[])
+	{
+		String a="copyandpastecopyandpastecopyandpastecopyandpastecopyandpastecopyandpastecommitandpushcommitandpushcommitandpushcommitandpushpushcccccommmitttsss";
+		System.out.println("The number of characters the sentence have are: "+a.length());
+	}
+
+}
