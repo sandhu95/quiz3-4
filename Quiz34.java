b8d9617a3c9683fe3879e6cd5a43b460c69d6d4d
Manpreet <Manpreet@DESKTOP-8VTP474>
2019-05-28 16:58:38 -0800

    part3

--- Quiz34.java
+++ Quiz34.java
@@ -7,5 +7,19 @@ public class Quiz34 {
 		//part2
 		String b= a.substring(a.length()-15);
 		System.out.println("The last 15 characters of the string are: "+b);
+		//part3
+		String[] items;
+		String z="";
+		items =a.split(z);
+		int count1=0,count2=0;
+		for(int i=0; i< items.length;i++)
+			{
+			if(items[i].compareTo("c")==0)
+				count1 +=1;
+			if(items[i].compareTo("p")==0)
+				count2 +=1;
+			}
+		System.out.println("The number of c's are: "+ count1);
+		System.out.println("The number of p's are: "+ count2);
 	}
 }
